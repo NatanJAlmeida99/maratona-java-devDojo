25336f5660909a7e7c25dfbf5df6936ef286386b
Natan Almeida <natanalmeida158@hotmail.com>
2026-01-31 14:13:08 -0300

    finish types

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -2,6 +2,17 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        long largeNumber = 100000;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500.0f;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean isTrue = true;
+        boolean isNotTrue = false;
+        char character = 'M';
+        System.out.println("Age is " + age + " years");
+        System.out.println(isNotTrue);
     }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Natan";
+        String address = "Rua Riachulo,844";
+        float salary = 2500.75f;
+        String date = "31/01/2026";
+
+        System.out.println("I " + name + " ,living in " + address + " confirm I received the salary " + salary + " in date " + date);
+    }
+}
